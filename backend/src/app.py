e21c31cb49ae33fa5d316228a4059d47f41ed00b
Alec Machlis <31748545+burturt@users.noreply.github.com>
2023-05-14 22:47:05 -0700

    Fix issue where react app only loaded on / and not other paths like /profile

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,22 +1,26 @@
 from flask import Flask
-from flask import send_file
+from flask import send_from_directory
+import os
 import redis
 import backend
 from config import config, rclient as r
 
-app = Flask(__name__, static_url_path="", static_folder="static")
-
-
-@app.route("/")
-def hello():
-    return send_file("static/index.html")
+app = Flask(__name__, static_folder="static")
 
 
 @app.route("/api/me")
 def hello_world():
-    count = r.incr("count")
+    count = r.incr("count") 
     return f"<p>Hello, World! {count}</p>"
 
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
