88d463caaa727f6057ff4f0ebdd6f1adf46543a8
Alexander Zhang <alex@alexyzhang.dev>
2023-05-15 14:48:24 -0700

    Migrate hooks from husky+lint-staged to pre-commit
    pre-commit will also be used for the backend.

--- frontend/lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    "src/**/*.ts?(x)": () => "tsc --noEmit",
-    "vite.config.ts": () => "tsc -p tsconfig.node.json --noEmit",
-    "**/*.[tj]s?(x)": "eslint --max-warnings 0",
-    "*": "prettier --check --ignore-unknown",
-};
