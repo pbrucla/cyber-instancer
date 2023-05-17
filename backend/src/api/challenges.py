b1779925640201213f570e68f4b93c1c011c697e
Alec Machlis <31748545+burturt@users.noreply.github.com>
2023-05-17 00:56:15 -0700

    Update challenges endpoint example response with multiple challenges in array

--- backend/src/api/challenges.py
+++ backend/src/api/challenges.py
@@ -12,6 +12,20 @@ def challenges():
             "tags": ["demo", "beginner"],
             "category": "web",
             "deployed": False,
-        }
+        },
+        {
+            "id": "test2",
+            "name": "Test chall 2",
+            "tags": ["demo", "advanced"],
+            "category": "pwn",
+            "deployed": True,
+        },
+        {
+            "id": "test3",
+            "name": "Test chall 3",
+            "tags": ["demo", "la ctf 2023"],
+            "category": "misc",
+            "deployed": False,
+        },
     ]
     return flask.jsonify(results=res)
