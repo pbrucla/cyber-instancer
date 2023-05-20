719c2cb946a3a734f6d1f11480fd8c1945265bc6
Alexander Zhang <alex@alexyzhang.dev>
2023-05-20 14:45:42 -0700

    Validate session token in API endpoints

--- backend/instancer/api/__init__.py
+++ backend/instancer/api/__init__.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request, g
 
 from . import account
+from . import authentication
 from . import challenge
 from . import challenges
 
@@ -8,3 +9,24 @@ blueprint = Blueprint("api", __name__)
 blueprint.register_blueprint(account.blueprint)
 blueprint.register_blueprint(challenge.blueprint, url_prefix="/challenge")
 blueprint.register_blueprint(challenges.blueprint, url_prefix="/challenges")
+
+
+@blueprint.before_request
+def check_authorization():
+    if request.endpoint in [
+        "api.account.register",
+        "api.account.login",
+        "api.account.dev_login",
+    ]:
+        return
+    if request.authorization is None:
+        return {"status": "unauthenticated", "msg": "authentication is required"}, 401
+    if request.authorization.type != "bearer":
+        return {
+            "status": "unauthenticated",
+            "msg": 'authorization type must be "Bearer"',
+        }, 401
+    session = authentication.get_session(request.authorization.token)
+    if session is None:
+        return {"status": "unauthenticated", "msg": "invalid token"}, 401
+    g.session = session
