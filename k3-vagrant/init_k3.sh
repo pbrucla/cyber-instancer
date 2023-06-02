eb4c57d51935e6df74d2eac7c60f2df1a044156f
Alec Machlis <31748545+burturt@users.noreply.github.com>
2023-06-02 12:26:56 -0400

    Update vagrant to use only one VM

--- k3-vagrant/init_k3.sh
+++ k3-vagrant/init_k3.sh
@@ -1,14 +1,46 @@
 #!/bin/bash
 
+# Run container registry server
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
+
+# Update fixture.sql to use our local docker registry and fake domain
+sed -i 's/docker.acmcyber.com/192.168.0.10/g' /tmp/fixture.sql
+sed -i 's/egg.gnk.sh/instancer.local/g' /tmp/fixture.sql
+
+# Run postgres server
+docker run -d -p 5432:5432 --restart=always --name db \
+  -e POSTGRES_PASSWORD=wLLaHYLzrV3j4yL7ErPkmhzf -e POSTGRES_USER=instancer \
+  -e POSTGRES_DB=instancer -v /tmp/fixture.sql:/docker-entrypoint-initdb.d/fixture.sql:ro postgres
+
+# Allow insure docker pushes to own repo
+cat << EOF > /etc/docker/daemon.json
+{
+  "insecure-registries" : ["http://192.168.0.10:5000"]
+}
+EOF
+
+# Restart docker
+systemctl restart docker
+
+# Build instancer docker container
+docker build /tmp/cyber-instancer/ -t 192.168.0.10:5000/cyber-instancer
+
+# Push to registry
+docker push 192.168.0.10:5000/cyber-instancer
+
+# Build and push example challenge docker image
+docker build /tmp/cyber-instancer/examples/simple-redis-chall -t 192.168.0.10:5000/simple-redis-chall
+docker push 192.168.0.10:5000/simple-redis-chall
+
 # install k3
 curl -sfL https://get.k3s.io | sh -
 
 # allow k3 unauthenticated docker pulls
 cat <<EOF >/etc/rancher/k3s/registries.yaml
 mirrors:
-  192.168.0.20:
+  192.168.0.10:
     endpoint:
-      - "http://192.168.0.20:5000"
+      - "http://192.168.0.10:5000"
 EOF
 
 systemctl restart k3s
@@ -77,7 +109,7 @@ stringData:
     redis:
       host: redis-service
     postgres:
-      host: 192.168.0.20
+      host: 192.168.0.10
       port: 5432
       user: instancer
       password: wLLaHYLzrV3j4yL7ErPkmhzf
@@ -111,7 +143,7 @@ spec:
       serviceAccountName: cyber-instancer
       containers:
         - name: app
-          image: 192.168.0.20/cyber-instancer:latest
+          image: 192.168.0.10/cyber-instancer:latest
           ports:
             - containerPort: 8080
           resources:
@@ -184,7 +216,7 @@ spec:
       serviceAccountName: cyber-instancer
       containers:
         - name: app
-          image: 192.168.0.20/cyber-instancer:latest
+          image: 192.168.0.10/cyber-instancer:latest
           ports:
             - containerPort: 8080
           resources:
--- k3-vagrant/init_reg.sh
+++ k3-vagrant/init_reg.sh
@@ -4,7 +4,7 @@
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 # Update fixture.sql to use our local docker registry and fake domain
-sed -i 's/docker.acmcyber.com/192.168.0.20/g' /tmp/fixture.sql
+sed -i 's/docker.acmcyber.com/192.168.0.10/g' /tmp/fixture.sql
 sed -i 's/egg.gnk.sh/instancer.local/g' /tmp/fixture.sql
 
 # Run postgres server
@@ -15,7 +15,7 @@ docker run -d -p 5432:5432 --restart=always --name db \
 # Allow insure docker pushes to own repo
 cat << EOF > /etc/docker/daemon.json
 {
-  "insecure-registries" : ["http://192.168.0.20:5000"]
+  "insecure-registries" : ["http://192.168.0.10:5000"]
 }
 EOF
 
@@ -23,11 +23,11 @@ EOF
 systemctl restart docker
 
 # Build instancer docker container
-docker build /tmp/cyber-instancer/ -t 192.168.0.20:5000/cyber-instancer
+docker build /tmp/cyber-instancer/ -t 192.168.0.10:5000/cyber-instancer
 
 # Push to registry
-docker push 192.168.0.20:5000/cyber-instancer
+docker push 192.168.0.10:5000/cyber-instancer
 
 # Build and push example challenge docker image
-docker build /tmp/cyber-instancer/examples/simple-redis-chall -t 192.168.0.20:5000/simple-redis-chall
-docker push 192.168.0.20:5000/simple-redis-chall
+docker build /tmp/cyber-instancer/examples/simple-redis-chall -t 192.168.0.10:5000/simple-redis-chall
+docker push 192.168.0.10:5000/simple-redis-chall
