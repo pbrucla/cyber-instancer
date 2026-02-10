cff2f267f0ea9f55112966ac20b15c837158f0e5
Aplet123 <aplet@aplet.me>
2026-02-09 18:36:16 -0800

    split categories and tags on comma instead of space

--- backend/instancer/api/admin/challenges.py
+++ backend/instancer/api/admin/challenges.py
@@ -150,8 +150,8 @@ def challenge_upload() -> ResponseReturnValue:
             description=request.form["description"],
             author=request.form["author"],
         )
-        categories = request.form["categories"].split()
-        other_tags = request.form["tags"].split()
+        categories = [x.strip() for x in request.form["categories"].split(",")]
+        other_tags = [x.strip() for x in request.form["tags"].split(",")]
         replace_existing = request.form.get("replace_existing", False)
     except (KeyError, ValueError):
         return {"status": "invalid_request", "msg": "invalid request"}, 400
